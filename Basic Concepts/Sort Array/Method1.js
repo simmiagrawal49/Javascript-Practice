bda72442eb5902e31c9855866e3738a207e9f958
simmiagrawal49 <55052741+simmiagrawal49@users.noreply.github.com>
2021-05-07 18:11:16 +0530

    Create Method1.js

---
+++ Basic Concepts/Sort Array/Method1.js
@@ -0,0 +1,7 @@
+//ascending Order
+var a = [ 1, 100, 50, 2, 5];
+a.sort(function(a,b) { return a - b; });
+
+//descending order
+var a = [ 1, 100, 50, 2, 5];
+a.sort(function(a,b) { return b - a; });
